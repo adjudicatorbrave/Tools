71ee69c7c79712492b958985a2704f31f51ae407
adjudicatorbrave <adjudicatorbrave@gmail.com>
2024-10-11 22:02:05 -0400

    update

--- nmap/nmap_scanner.sh
+++ nmap/nmap_scanner.sh
@@ -8,9 +8,9 @@ fileName=$3
 echo "Executing nmap scans on IP address "$ip" at a rate of T"$rate" and storing into files labelled"$fileName
 
 
-nmap_syn_command="nmap -T"$rate" -p- -sS -oN "$fileName"_syn_scan "$ip
-nmap_ack_command="nmap -T"$rate" -p- -sA -oN "$fileName"_ack_scan "$ip
-nmap_tcp_command="nmap -T"$rate" -p- -sT -oN "$fileName"_tcp_scan "$ip
+nmap_syn_command="nmap -T"$rate" -p- -sS -oX "$fileName"_syn_scan "$ip
+nmap_ack_command="nmap -T"$rate" -p- -sA -oX "$fileName"_ack_scan "$ip
+nmap_tcp_command="nmap -T"$rate" -p- -sT -oX "$fileName"_tcp_scan "$ip
 
 echo $nmap_syn_command
 $nmap_syn_command &
